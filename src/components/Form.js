b7f34a9e86bf93ee0e036d2ba7a18ff68619ccc1
RoyOkite <royokite@gmail.com>
2022-09-25 16:29:24 +0300

    Code Along; Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState("Rokyt");
+  const [lastName, setLastName] = useState("101");
+  const [formData, setFormData] = useState([])
+  const [error, setError] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,46 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    if(firstName.length > 1) {
+      const newSubmit = { firstName, lastName}
+      const submitList = [...formData, newSubmit]
+      setFormData(submitList)
+      setFirstName('')
+      setLastName('')
+      setError([])
+    } else {
+      setError(["First name is required!"])
+    }
+  }
+
+  const allSubmissions = formData.map(submission => {
+    return (
+      <div key={submission.firstName}>
+        {submission.firstName} {submission.lastName}
+      </div>
+    )
+  })
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {error.length > 0
+      ? error.map((eachError) => (
+          <p key={eachError} style={{ color: "red" }}>
+            {eachError}
+          </p>
+        ))
+      : null}
+      <h3>Sumissions</h3>
+      {allSubmissions}
+    </div>
   );
 }
 
